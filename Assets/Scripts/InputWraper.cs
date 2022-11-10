6c57055852a2ca9b3d8fe6d352d7f800d4a1bfb4
AlanBuechner <47373257+AlanBuechner@users.noreply.github.com>
2022-11-10 11:08:39 -0700

    added editor support for input

--- Assets/Scripts/InputWraper.cs
+++ Assets/Scripts/InputWraper.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InputWraper
 {
 	public static bool GetInputLocationOnRect(RectTransform rect, out Vector2 tapPosition)
 	{
+		bool isClicked = Input.GetMouseButtonDown((int)MouseButton.Left);
+		bool isTouch = Input.touchCount > 0;
 
-		if (Input.touchCount > 0)
+		if (isClicked || isTouch)
 		{
-			Touch touch = Input.GetTouch(0);
+			Vector2 touchPosition = isClicked ? (Vector2)Input.mousePosition : Input.GetTouch(0).position;
 			Vector2 position;
-			if(RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, touch.position, null, out position))
+			if(RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, touchPosition, null, out position))
 			{
 				if (position.x >= rect.rect.xMin && position.x <= rect.rect.xMax &&
 					position.y >= rect.rect.yMin && position.y <= rect.rect.yMax)
 				{
 					position.x -= rect.rect.xMin;
-					position.y -= rect.rect.yMin;
+					position.y = rect.rect.yMax - position.y;
 					tapPosition = position;
 					return true;
 				}
